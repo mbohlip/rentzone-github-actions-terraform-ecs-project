084d07d5e8471a67408ba261cd793ee9c8174c22
Peter Mbohli <mbohlip@yahoo.com>
2024-01-22 09:56:15 -0700

    commit

--- iac/07_ecs-role.tf
+++ iac/07_ecs-role.tf
@@ -31,7 +31,7 @@ data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
     ]
 
     resources = [
-      "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}/*"
+      "arn:aws:s3:::${var.project_name}-${var.environment}/*"
     ]
   }
 
@@ -41,7 +41,7 @@ data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
     ]
 
     resources = [
-      "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}"
+      "arn:aws:s3:::${var.project_name}-${var.environment}"
     ]
   }
 }
