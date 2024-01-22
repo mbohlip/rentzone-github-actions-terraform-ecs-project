390245a25933d77144f005306fdce4e2330a54a4
Peter Mbohli <mbohlip@yahoo.com>
2024-01-22 11:52:49 -0700

    commit

--- iac/10_ecs.tf
+++ iac/10_ecs.tf
@@ -47,7 +47,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
 
       environmentFiles = [
         {
-          value = "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}/${var.env_file_name}"
+          value = "arn:aws:s3:::${var.project_name}-${var.environment}/${var.env_file_name}"
           type  = "s3"
         }
       ]
